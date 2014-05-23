06173d9725a509fbc0dab4253a79d4f4363605be
Martin Becker <mbeckerwork@gmail.com>
2014-05-23 23:31:38 +0100

    changed redis address

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
 	port = 9994
-	host =  URI.parse(ENV["REDISTOGO_URL"])
+	host =  URI.parse('redis://redistogo:826b21c287cb32ca1237d5493b8881d3@angelfish.redistogo.com:9994/') #URI.parse(ENV["REDISTOGO_URL"])
 else
 	port = 6379
 	host = 'localhost'

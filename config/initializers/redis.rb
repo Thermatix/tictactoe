04506c4e7a1acc3aa2e9288698f18ae9b30767da
Martin Becker <mbeckerwork@gmail.com>
2014-05-23 18:45:31 +0100

    Adds redis host for deployment

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,8 @@
 if Rails.env.production?
 	port = 9994
+	host =  URI.parse(ENV["REDISTOGO_URL"])
 else
 	port = 6379
+	host = 'localhost'
 end
-$redis = Redis.new(host: 'localhost', port:  port )
+$redis = Redis.new(host: host , port:  port )

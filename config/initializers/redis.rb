39ac5e38d47da6bf0432300cd86c58345f5fa1b7
Martin Becker <mbeckerwork@gmail.com>
2014-05-23 23:38:40 +0100

    Changes Redis setup to be more concise

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,2 @@
-if Rails.env.production?
-	# port = 9994
-	uri = URI.parse(ENV["REDISTOGO_URL"])
-else
-	port = 6379
-	host = 'localhost'
-end
-$redis = Redis.new(host: uri.host || host, port: uri.port || port, password: uri.password || nil)
-Resque.redis = $redis
+uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)

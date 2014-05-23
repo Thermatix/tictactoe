f3024dbe0dfe3605043c323870832eea81348798
Martin Becker <mbeckerwork@gmail.com>
2014-05-23 23:36:59 +0100

    Adds redis setup

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,9 @@
 if Rails.env.production?
-	port = 9994
-	host =  URI.parse('redis://redistogo:826b21c287cb32ca1237d5493b8881d3@angelfish.redistogo.com:9994/') #URI.parse(ENV["REDISTOGO_URL"])
+	# port = 9994
+	uri = URI.parse(ENV["REDISTOGO_URL"])
 else
 	port = 6379
 	host = 'localhost'
 end
-$redis = Redis.new(host: host , port:  port )
+$redis = Redis.new(host: uri.host || host, port: uri.port || port, password: uri.password || nil)
+Resque.redis = $redis
